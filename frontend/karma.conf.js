b0ca47443b16c3c9d83d5ca60908f71d5cf58e9f
AviMax Dev Team <dev@avimax.local>
2026-05-14 23:00:50 -0600

    ci: arreglar chrome headless en frontend

---
+++ frontend/karma.conf.js
@@ -0,0 +1,40 @@
+const path = require('node:path');
+
+function karmaConfig(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, './coverage/frontend'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+      },
+    },
+    singleRun: true,
+    restartOnFileChange: false,
+  });
+}
+
+module.exports = karmaConfig;
